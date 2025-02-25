049830ae30dc2df2540c9e132022c26ed8cd1990
Emily Wade <emilywade@Emilys-MacBook-Air.local>
2025-02-25 10:36:00 +0000

    adding event_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Post, Event
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -31,4 +31,21 @@ def post_detail(request, slug):
         request,
         "blog/post_detail.html",
         {"post": post},
+    )
+
+class EventsList(generic.ListView):
+
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
     )
